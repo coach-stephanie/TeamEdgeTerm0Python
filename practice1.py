cdf43ba62f07a27085ceb65566cae3418e4880c2
Cmag34 <chantel.mager@csedge.org>
2022-07-07 11:25:33 -0400

    lab 1 challenges

---
+++ practice1.py
@@ -0,0 +1,16 @@
+age = 17
+
+name = 'Chantel'
+
+height = 5
+
+
+print("Hello Wolrd")
+print(":D")
+print(age)
+
+#This prints my age 
+print (f'My name is {name} and my age is {age}')
+print(f'My height is {height}')
+
+print ("My name is Chantel and my age is {age} ")
