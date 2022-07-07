7ef6af4ce4a91e93b3c07df9cbb50002275bf9ff
blancamr3 <blanca@csedge.org>
2022-07-07 11:46:04 -0400

    updayes

---
+++ practice1.py
@@ -0,0 +1,12 @@
+age = 17
+
+name = 'Marleny'
+
+print ('Hello world')
+print(age)
+
+#This is my age by formatting the string
+print('17 + name')
+
+print(f'My age is {age}')
+print(f'My name is {name} and my age is {age}')
