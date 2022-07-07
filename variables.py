2680969b12bce53ffe016f62191ac57bef75544c
CsMuj <108821370+CsMuj@users.noreply.github.com>
2022-07-07 11:45:19 -0400

    Add files via upload

--- variables.py
+++ variables.py
@@ -4,7 +4,7 @@
 # -------------------------------------------- 
 
 # Example
-message = "Hello World!"
+message = "\nHello World!"
 print(message)
 
 # -------------------------------------------- 
@@ -18,14 +18,20 @@ print(message)
 
 print("\n------------------- Challenge 1 -------------------\n")
 
-#print(f"Hi! My name is {name}")
-#print(f"I'm in the {grade}th grade.")
-#print(f"A fun fact about me is that {funFact}")
+name = 'Mujtaba'
+grade = 10
+funFact = 'I do not like spice'
+
+print(f"Hi! My name is {name}")
+print(f"I'm in the {grade}th grade.")
+print(f"A fun fact about me is that {funFact}")
+print(f'\nHappy Birthday to you dear \n{name} \nAre you 1, 2, 3')
 
 
 # **** Challenge 1: Problem 1 ****
 # Initialize the variables used in the {} with your data for the three statements above! 
 # Hint: Also think about where you would initialize those variables
+
 
 
 # **** Upchallenge! ****
@@ -58,42 +64,54 @@ print("\n------------------- Challenge 2 -------------------\n")
 # Here are some variables to get you started
 num1 = 5
 num2 = 10
+num3 = num1 + num2
+num4 = 7
+num5 = num3 - num4
+num6 = num5 * 3
+num7 = num6**2
+num8 = num7 / 4
+num9 = num8 % 2
+num10 = 9
+num11 = num10 - 1
+num12 = num10 +1
 
+print(f'Answers to the challanges\n')
 # **** Challenge 2: Problem 1 ****
 # Store the sum of num1 and num2 in a variable.
 # Print the sum.
-
+print(f'Problem 1 {num1} + {num2} = {num3}')
 
 # **** Challenge 2: Problem 2 ****
 # Store the difference between sum (above) and 7 in a variable.
 # Print the difference.
-
+print(f'Problem 2 {num3} - {num4} = {num5}')
 
 # **** Challenge 2: Problem 3 ****
 # Store the product of the difference and 3 in a variable.
 # Print the product.
-
+print('Problem 3', num6)
 
 # **** Challenge 2: Problem 4 ****
 # Store the power of the product squared in a variable.
 # Print the power.
-
+print('Problem 4', num7)
 
 # **** Challenge 2: Problem 5 ****
 # Store the quotient of the power divided by 4 in a variable.
 # Print the quotient.
-
+print('Problem 5', num8)
 
 # **** Challenge 2: Problem 6 ****
 # Store the remainder of the quotient divided by 2 in a variable.
 # Print the remainder.
-
+print('Problem 6', num9)
 
 # **** Upchallenge! ****
 # Given a variable that stores a number, print the values that come right before and right after.
 # For example, given the number 4, the program should print 3 and 5. 
 # Your code should work even if the value of the variable is changed.
 
+print(f'Upchallenge {num11} and {num12}\n')
 num = 9
 
 # -------------------------------------------- 
