a8b8f5e8e13e862070a8848b12a3a53f4dc45bc6
Cmag34 <104530321+Cmag34@users.noreply.github.com>
2022-07-08 11:57:49 -0400

    Update conditional_game.py

--- conditional_game.py
+++ conditional_game.py
@@ -31,7 +31,9 @@ import random
 
   # --------------------------------------------
 
-
+print("How a Magic 8 Ball works:")
+print("The user asks a question and vigorously shakes the ball.")
+print("Then the ball gives the user gets a fortune")
 
 
 
@@ -49,32 +51,56 @@ import random
 
 	# Part 2: Next, we need to randomly select a response from 20 options.
 
-	# Randomly select a number from 0 - 19 
+	# Randomly select a number from 0 - 19       
 	# Use that to select from the following responses:
-		# 0 - It is certain.
-		# 1 - It is decidedly so.
-		# 2 - Without a doubt.
-		# 3 - Yes - definitely.
-		# 4 - You may rely on it.
-		# 5 - As I see it, yes.
-		# 6 - Most likely.
-		# 7 - Outlook good.
-		# 8 - Yes.
-		# 9 - Signs point to yes.
-		# 10 - Reply hazy, try again.
-		# 11 - Ask again later.
-		# 12 - Better not tell you now.
-		# 13 - Cannot predict now.
-		# 14 - Concentrate and ask again.
-		# 15 - Don't count on it.
-		# 16 - My reply is no.
-		# 17 - My sources say no.
-		# 18 - Outlook not so good.
-		# 19 - Very doubtful.
+	select=random.randrange(1,20)
+	if  select=0
+	print(" It is certain.")
+	elif  select=1
+	print("It is decidedly so.")
+	elif  select=2
+	print("Without a doubt.")
+	elif  select=3
+	print("Yes - definitely.")
+	elif  select=4
+	print("You may rely on it.")
+	elif  select=5
+	print("As I see it, yes.")
+	elif  select=6
+	print("Most likely.")
+	elif  select=7
+	print("Outlook good.")
+	elif  select=8
+	print("Yes.")
+	elif  select=9
+	print("Signs point to yes.")
+	elif  select=10
+	print("Reply hazy, try again.")
+	elif  select=11
+	print("Ask again later.")
+	elif  select=12
+	print("Better not tell you now.")
+	elif  select=13
+	print("Cannot predict now.")
+	elif  select=14
+	print("Concentrate and ask again.")
+	elif  select=15
+	print("Don't count on it.")
+	elif  select=16
+	print("My reply is no.")
+	elif  select=17
+	print("My sources say no.")
+	elif  select=18
+	print("Outlook not so good.")
+	elif  select=19
+	print("Very doubtful.")
+		
 
 	# Look up random.rand_int to see how you can use it to select a random number.
 
   # -------------------------------------------- 
+
+
 
 
 
@@ -105,7 +131,48 @@ import random
 
 
 
-
+select=random.randrange(1,20)
+	if  select=0
+	print(" you suck.")
+	elif  select=1
+	print("unlikely.")
+	elif  select=2
+	print("no.")
+	elif  select=3
+	print("nope.")
+	elif  select=4
+	print("never.")
+	elif  select=5
+	print("this leads to a fatal ending.")
+	elif  select=6
+	print("hopefully.")
+	elif  select=7
+	print("sadly so.")
+	elif  select=8
+	print("Yes.")
+	elif  select=9
+	print("maybe.")
+	elif  select=10
+	print("Reply hazy, try again.")
+	elif  select=11
+	print("Ask again later.")
+	elif  select=12
+	print("Better not tell you now.")
+	elif  select=13
+	print("the future will tell you.")
+	elif  select=14
+	print("the past")
+	elif  select=15
+	print("Don't count on it.")
+	elif  select=16
+	print("My reply is no.")
+	elif  select=17
+	print("My sources say deffinetly not.")
+	elif  select=18
+	print("not likely my friend.")
+	else:
+	print("Very doubtful.")
+		
 
 
 
