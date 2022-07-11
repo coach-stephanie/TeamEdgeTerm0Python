44fdfcfe40275672a9c8b9721c4aebc6822056c2
KaleTheFirst <kyle.brooks@csedge.org>
2022-07-11 11:31:48 -0400

    variables

--- variables.py
+++ variables.py
@@ -17,7 +17,9 @@
 # -------------------------------------------- 
 
 #print("\n------------------- Challenge 1 -------------------\n")
-
+#name = "Kyle"
+#grade = "10"
+#funFact = "I like to skateboard"
 #print(f"Hi! My name is {name}")
 #print(f"I'm in the {grade}th grade.")
 #print(f"A fun fact about me is that {funFact}")
@@ -38,9 +40,10 @@
 # Create a string for the different lines of the Happy Birthday Song
 # Print out the song using the strings you declared.
 
+Line1 = "Happy Birthday to you\n"
+Line2 = "How Old Are You Now?"
 
-
-
+print(Line1 + Line2)
 # **** Challenge 1: Problem 3 ****
 # Print out a string that has brackets in it.
 
@@ -53,7 +56,7 @@
 # Write your solution code under each commented problem. 
 # -------------------------------------------- 
 
-print("\n------------------- Challenge 2 -------------------\n")
+#print("\n------------------- Challenge 2 -------------------\n")
 
 # Here are some variables to get you started
 num1 = 5
@@ -62,39 +65,49 @@ num2 = 10
 # **** Challenge 2: Problem 1 ****
 # Store the sum of num1 and num2 in a variable.
 # Print the sum.
-sum1 = (num1 + num2)
-print(sum1)
+sum = 15
+print(sum)
+
 # **** Challenge 2: Problem 2 ****
 # Store the difference between sum (above) and 7 in a variable.
 # Print the difference.
-sum2 = (sum1 -7)
-print(sum2)
+num3 = 7
+diff = 8
+print(sum - num3)
 
 # **** Challenge 2: Problem 3 ****
 # Store the product of the difference and 3 in a variable.
 # Print the product.
-
-
+num4 = 3
+print(diff*num4)
 # **** Challenge 2: Problem 4 ****
 # Store the power of the product squared in a variable.
 # Print the power.
+product = 24
+print(product*product)
 
 
 # **** Challenge 2: Problem 5 ****
 # Store the quotient of the power divided by 4 in a variable.
 # Print the quotient.
-
-
+power = 576
+num5 = 4
+print(power/num5)
 # **** Challenge 2: Problem 6 ****
 # Store the remainder of the quotient divided by 2 in a variable.
 # Print the remainder.
+quotient = 144.0
+num6 = 2 
 
+print(quotient%num6)
 
 # **** Upchallenge! ****
 # Given a variable that stores a number, print the values that come right before and right after.
 # For example, given the number 4, the program should print 3 and 5. 
 # Your code should work even if the value of the variable is changed.
 
+new1 = 1
 num = 9
-
+print(num-new1)
+print(num+new1)
 # -------------------------------------------- 
