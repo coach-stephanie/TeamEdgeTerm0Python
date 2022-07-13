c8d11085be8e8afe85ce9d0d89eb67fc0745dae4
Natanael <Natanael-Mota15>
2022-07-13 15:43:10 -0400

    lab 6 challenges

--- lists.py
+++ lists.py
@@ -28,25 +28,30 @@ names = ["Julian", "Wolf", "Alex", "Steph", "Alessandro"]
 print("names: " + str(names))  
 
 #-->TODO: Declare another list called friends with at least 5 strings inside (if you don't have 5 friends make them up!)
-
+people = ["Austen", "jack", "Joni", "Benny", "Eli"]
+print("my name" + str(people))
 
 #this list holds numbers
 numbers = [12.9, 23.4 , 100, 3.1415 , 500, 1.20]
 print("numbers: " + str(numbers)) 
 
 #-->TODO: Declare another list and add in at least 5 numbers. Why five? I don't know. It just feels right.
-
-
+num = [1, 2, 3, 4, 5]
+print("my numbers" + str(num))
 
 #this list has mixed data types. It's allowed in Python!
 random_stuff = ["Aardvark", True, False, 1.23, "Grandpa"]
 print("random: " + str(random_stuff))
 
 #-->TODO: Declare and log a list filled with the first 5 things that come into your head, booleans, Strings, numbers are all cool,
-
+bla = ["p", 345, "blablabla", "-_-", "wow", 3939393]
+print("random things" + str(bla))
 
 #-->TODO: Declare and log two more lists with whatever you want. 
-
+best_things = ["good soda", 15, "games", "life", " in some cases money"]
+bad_thing = ["fees", "depts", "bad grades", 0, "grounds"]
+print ("best things:" + str(best_things))
+print ("bad things" + str(bad_thing))
 
 print("------------------- CHALLENGE 2 -------------------")
  
@@ -54,10 +59,10 @@ print("------------------- CHALLENGE 2 -------------------")
 print("The first name is " + names[0])
 
 #-->TODO: Print the name of your best friend from your friends list
-
+print("Best freind:" + people[0])
 
 #-->TODO: Print the first AND last elements of any list you made, or make a brand new one.
-
+print("things:" + best_things[0] + best_things[4])
 
 print("------------------- CHALLENGE 3 -------------------")
 #this code changes the value of the second element of the names list, then we print the list:
@@ -65,7 +70,12 @@ names[1] = "Alyssa"
 print(names)
 
 #-->TODO: Replace your friends! Modify the list to replace any or all of your friends with new ones.
-
+people[0] = "Gaby"
+people[1] = "Gio"
+people[2] = "Daniel"
+people[3] = "Amy"
+people[4] = "Andrea"
+print(people)
 
 #The code below uses the times_ten() function to multiply the first element in our list by 10:
 def times_ten(number):
@@ -77,9 +87,21 @@ print(numbers)
 
 #-->TODO: Write another function that multiplies a number by 1000 and print the list, as above 
 
+def time(num):
+    num = num * 1000
+    return num
 
+num[0] = time(num[0])
+print(num)
 
 #-->TODO: Replace your random list elements with anything you want, using the index. 
+best_things[0] = "howl"
+best_things[1] = "charzard"
+best_things[2] = "pokeball"
+best_things[3] = "masterball"
+best_things[4] = "boom"
+
+print (str(best_things))
 
 print("------------------- CHALLENGE 4 -------------------")
 
@@ -90,7 +112,8 @@ parent_list = [child_list_1, child_list_2]
 print("This list has babies: " + str(parent_list))
 
 #-->TODO: Store and print all the lists we have worked on thus far in a new parent list
-
+lists = [best_things, bad_thing, num, people]
+print (str(lists))
 
 print("------------------- CHALLENGE 5 -------------------")
 
@@ -100,11 +123,14 @@ print("Movies: " + str(movies))
 movies.append("Joker")
 movies.append("Black Panther")
 print("Movies now has: " + str(movies))
+lists.append("awesome")
+print(lists)
 
 #-->TODO: Declare a list with 5 favorite songs
-
+fav_songs = ["7 years", "cold as ice", "believer", "something just like this", "sky full of stars"]
 #-->TODO: Add 2-3 more songs using .append() and print both before and after.
-
+fav_songs.append("falling")
+fav_songs.append("just so you know")
 #We can also remove elements using .pop(), which removes the last element or the element at the given index. You can store it after it comes out:
 cities = ["New York", "Oakland", "Las Vegas", "Topeka"]
 print("cities: " + str(cities))
@@ -112,7 +138,9 @@ unwanted_city = cities.pop()
 print("unwanted city: " + str(unwanted_city))
 
 #-->TODO: remove your last song using .pop() and print the removed element as above
-
+print("fav_songs: " + str(fav_songs))
+unwanted_song = fav_songs.pop()
+print("unwanted song: " + str(unwanted_song))
 #Note: there are more methods to remove and modify list elements. We will cover those later
 
 print("------------------- CHALLENGE 6 -------------------")
@@ -124,7 +152,7 @@ how_many_cities = len(cities)
 print(f"There are {how_many_cities} ciites in my list")
 
 #-->TODO: Print out the number of friends, or other items from other lists using string literals as above
-
+print("i have " + str(len(people))  + " friends")
 
 #The len() function is key, especially in conditionals or to simply count how many times to do something.
 
@@ -134,6 +162,11 @@ else:
     print("I need more numbers in my list!!!")
 
 #-->TODO: Write another if/else statement to check the size of your songs list. If you have 5 of less, add two more songs!
+if len(fav_songs) < 5:
+    fav_songs.append("Stiches")
+    fav_songs.append("Enemies")
+else:
+    print("Good Job")
 
 
 print("------------------- CHALLENGE 6 -------------------")
@@ -148,15 +181,26 @@ word_split_list = word.split() #split by nothing, and you get each character in 
 print("letter by letter: " + str(word_split_list))
 
 #using this you can split strings up by any character!
+bleb = "bla ble blu"
+bleb_split = bleb.split()
+print (bleb_split)
 
 #-->TODO: Change the name of the person who is late in this sentence and print it.
 split_me = "I heard Alex was late to class today."
+splitted_me = split_me.split()
+splitted_me[2] = "Bob"
+print (str(splitted_me))
 
 #-->TODO: Add an exclamation mark to this sentence using split() and append(), then print. (yes, there are other ways, but...)
 make_me_exciting = "What a wonderful day"
-
+use_these_kinds_variables = make_me_exciting.split()
+use_these_kinds_variables.append("!")
+print(use_these_kinds_variables)
 #We can also join our list elements into a string using.....join()!
 rejoined = " ".join(boring_list)  #joins it using spaces
 print('back in one piece: ' + rejoined)
 
 #-->TODO:  Finally, put the split_me sentence today and the make_me_exciting strings back together and print. You should see a string
+xy = " ".join(splitted_me)  
+xt = " ".join(use_these_kinds_variables) 
+print('back in one piece: ' + xy + xt)
--- shopping_list.py
+++ shopping_list.py
@@ -11,37 +11,72 @@
  #  again, giving feedback along the way. 
  # 
  # ***************************************************************/
+import random
 
 active = True
 
 print("Welcome to Shopping List!")
 
-welcome_message = "Hi! I'm your shopping assistant. Let me take your order. \n You can type 'add milk' to add milk to your shopping list. \n or you can type 'remove milk' to remove it. \n"
+welcome_message = ("Hi! I'm your shopping assistant. Let me take your order. \n You can type 'add milk' to add milk to your shopping list. \n or you can type 'remove milk' to remove it. \n")
 
-print(welcome_message)
+
+
 
 
 #-->Todo: declare a shopping_list list
+list = []
+
+
 
 
 def prompt_user():
 
     reply = input("What do you want to add or remove?  >>  ")
-
+    if reply == "add":
+        add_item()
+    elif reply == "remove":
+        remove_item()
+    else:
+        print("not an option, be more clear")
     return reply
 
-def check_answer(ans):
+
+def check_answer():
+    num = random.randrange(1,2) 
+    if num == 1:
+        print(welcome_message)
+    elif num == 2:
+        print("Hi! I'm your shopping assistant. Let me take your order. \n Just choose between add or remove and order")
     pass
 
 
 def add_item():
 #this function can take in a string and store it in an array
+    y = input("What do you want to add?")
+    if y in list:
+        print("You already have that")
+    else:
+        list.append(y)
+        print (list)
+        print (f"Amount of items:{len(list)}")
     pass
 
 
 def remove_item():
+    x = input("What do you want to remove?")
+    list.remove(x)
+    print (list)
+    print (f"Amount of items:{len(list)}")
     pass
 
-while active:
 
+prompt_user()
+while active:
+    list.sort()
     check_answer(prompt_user()) #this makes the program continously prompt and check response while the boolean 'active' returns True
+
+
+
+
+
+check_answer()
